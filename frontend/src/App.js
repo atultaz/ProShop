3ee64e3ef452f6f6a9f7d6d3cba4bc228d4ae383
atultaz <atul.tiwari@me.com>
2021-08-18 11:26:34 +0530

    commint on 'main'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <Fragment>
-      <h1>Welcome To Proshop</h1>
-    </Fragment>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
